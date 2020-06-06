9c73ad68916d44edb9a9402d8016e00d971ee78b
th3nn3ss <chuksmcdennis@yahoo.com>
2020-06-06 23:27:18 +0100

    set environ

--- bot_tweets/config.py
+++ bot_tweets/config.py
@@ -5,10 +5,10 @@ import os
 logger  = logging.getLogger()
 
 def create_api():
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+    consumer_key = os.environ["CONSUMER_KEY"]
+    consumer_secret = os.environ["CONSUMER_SECRET"]
+    access_token = os.environ["ACCESS_TOKEN"]
+    access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
