e3a7b4e57a9c5788b69af19bff1bf9d9b047f882
th3nn3ss <chuksmcdennis@yahoo.com>
2020-06-07 16:20:39 +0100

    fix

--- hashtags.py
+++ hashtags.py
@@ -4,10 +4,10 @@ from time import sleep
 import os
 
 # Access and authorize our Twitter credentials from credentials.py
-consumer_key = os.environ['CONSUMER_KEY']
-consumer_secret = os.environ['CONSUMER_SECRET']
-access_token = os.environ['ACCESS_TOKEN']
-access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+consumer_key = os.environ.get['CONSUMER_KEY']
+consumer_secret = os.environ.get['CONSUMER_SECRET']
+access_token = os.environ.get['ACCESS_TOKEN']
+access_token_secret = os.environ.get['ACCESS_TOKEN_SECRET']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
